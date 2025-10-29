b961b08e11a01b2386bb0ec46f0bd1cb28c99afd
Cardenas123 <cardenashillary22@gmail.com>
2025-10-29 11:03:14 -0500

    feat: Creando configuracion inicial de React

---
+++ src/frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+function Home(){
+    return(
+        <div className="min-h-screen bg-gray-100 p-8">
+            <h1 className="text-2xl font-bold mb-4">Bienvenido a mi pagina 🌻</h1>
+            <p className="text-orange-600 text-xl">Esta es nuestra aplicacion con React</p>
+
+        </div>
+
+    );
+}
+
+export default Home;
